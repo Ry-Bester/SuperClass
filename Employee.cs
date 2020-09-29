15bc429eae0f81bea2b1e3324b150e003aeb4236
Ryan <zelda0169@gmailcom>
2020-09-29 17:05:56 -0600

    added the overlaoded == operator

--- Employee.cs
+++ Employee.cs
@@ -10,5 +10,33 @@ namespace SuperClass
      public class Employee : Person
     {
         public int Id { get; set; }
+
+        public static bool operator == (Employee obj1 , Employee obj2 )
+        {
+            bool result;
+            if (obj1.Id == obj2.Id)
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        public static bool operator != (Employee obj1, Employee obj2)
+        {
+            bool result;
+            if (obj1.Id != obj2.Id)
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+            return result;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,9 +11,16 @@ namespace SuperClass
         static void Main(string[] args)
         {
             Employee employee = new Employee();
-            employee.FirstName = "Sample";
-            employee.LastName = "Student";
+            employee.FirstName = "Joe";
+            employee.LastName = "Blow";
+            employee.Id = 1;
+            Employee employee1 = new Employee();
+            employee1.FirstName = "Billy";
+            employee1.LastName = "Bob";
+            employee1.Id = 2;
             employee.SayName();
+            Console.WriteLine(employee == employee1);
+            Console.WriteLine(employee != employee1);
             Console.ReadLine();
         }
     }
